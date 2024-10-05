a5c1f6db9860c9526ed89538820206c7118041dc
Neto Flavio <95658733+zNetinho@users.noreply.github.com>
2024-10-05 08:56:11 -0300

    feat: created form and validating data with zod

--- src/app/admin/_components/form-login.tsx
+++ src/app/admin/_components/form-login.tsx
@@ -1,52 +1,84 @@
+"use client"
+
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { cn } from '@/lib/utils'
 import { GenericsProps } from '@/types/common'
-import React from 'react'
+import React, { LegacyRef } from 'react'
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { UserSchema, UserSchemaType } from '@/schemas/login-schema'
 
 function FormLogin() {
+
+    const { 
+        register,
+        handleSubmit,
+        formState: { errors },
+        control
+    } = useForm<UserSchemaType>({
+        resolver: zodResolver(UserSchema)
+    })
+
+    const onSubmit: SubmitHandler<UserSchemaType> = (data) => console.log(data)
+    const tester = (data: UserSchemaType, e: any) => {
+        e.preventDefault()
+        onSubmit(data)
+        console.log("Evento de submit");
+        console.log(data);
+      };
+    
     return (
         <form
-            action=""
-            method="post"
             className='flex flex-col gap-3'
+            onSubmit={handleSubmit(tester)}
         >
             <div>
                 <LabelComponent
-                    htmlFor="email"
-                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                    htmlFor='emailInput'
+                    className='text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
                 >
                     Email
                 </LabelComponent>
-                <InputComponent
-                    type="text"
-                    name="email"
-                    id="email"
-                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                <input
+                    type='text'
+                    id='emailInput'
+                    {...register("email")}
+                    className='py-1 px-1 w-full focus-visible:outline-0 focus-visible:border-0 text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
+                    
                 />
+                {errors.email && <p>{errors.email?.message}</p>}
             </div>
             <div>
                 <LabelComponent
-                    htmlFor="password"
+                    htmlFor='password'
                 >
                     Password
                 </LabelComponent>
-                <InputComponent type="text" name="password" id="password" />
+                <input 
+                    type='password'
+                    id='passwordInput'
+                    className='py-1 px-1 w-full focus-visible:outline-0 focus-visible:border-0 text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
+                    {...register("password")}
+                />
 
             </div>
-                <ButtonComponent>
-                    Acessar
-                </ButtonComponent>
+            <ButtonComponent
+                type='submit'
+            >
+                Acessar
+            </ButtonComponent>
         </form>
     )
 }
 
 type LabelProps = {
-    htmlFor?: string
+    htmlFor: string
 }
 
-function LabelComponent({ children, className, htmlFor }: GenericsProps<LabelProps>) {
+function LabelComponent({ children, className, htmlFor = 'undefined' }: GenericsProps<LabelProps>) {
     return (
         <Label
             htmlFor={htmlFor}
@@ -61,23 +93,36 @@ type InputProps = {
     type?: string
     name?: string
     id?: string
+    ref: LegacyRef<HTMLInputElement>
 }
 
-function InputComponent({ className, id, name, type }: GenericsProps<InputProps>) {
+function InputComponent({ className, id = 'undefined', type = 'text', rest }: GenericsProps<InputProps>) {
+
+    const { 
+        register,
+    } = useForm<UserSchemaType>({
+        resolver: zodResolver(UserSchema)
+    })
+
     return (
-        <Input
+        <input
             type={type}
-            name={name}
             id={id}
-            className={cn(['text-sm font-medium py-1 px-1 w-full focus-visible:outline-0', className])}
+            className={cn(['text-sm font-medium py-1 px-1 w-full focus-visible:outline-0 focus-visible:border-0', className])}
+            {...rest}
         />
     )
 }
 
-function ButtonComponent({ children, className }: GenericsProps) {
+type ButtonProps = {
+    type?: 'submit' | 'button' | 'reset'
+}
+
+function ButtonComponent({ children, className, type = "button" }: GenericsProps<ButtonProps>) {
     return (
         <Button
             className={cn(['w-full', className])}
+            type={type}
         >
             {children}
         </Button>
