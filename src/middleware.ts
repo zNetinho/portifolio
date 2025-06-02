1a8f2bb0931347b0489600678c1a730c81387247
zNetinho <netoflavio97@hotmail.com>
2025-06-01 21:11:05 -0300

    hotfix: alter middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,12 +2,7 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher(['/admin(.*)', '/post(.*)'])
 
-export default clerkMiddleware((auth, req) => {
-
-    if (isProtectedRoute(req)) {
-        auth().protect()
-    }
-});
+export default clerkMiddleware();
 
 export const config = {
     matcher: [
