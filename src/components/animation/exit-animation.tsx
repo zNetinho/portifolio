56a909feae3dfe0607e75423354979889837e136
zNetinho <netoflavio97@hotmail.com>
2025-06-17 20:39:04 -0300

    hotfix: corrigido animação de scrollTo e do menu.

--- src/components/animation/exit-animation.tsx
+++ src/components/animation/exit-animation.tsx
@@ -37,24 +37,28 @@ export default function ExitAnimation() {
     window.scrollTo(0, 0);
   }, []);
 
+  const handleToggle = () => {
+    setToggle(!toggle)
+    setIsVisible(!isVisible)
+  }
+
   return (
     <div className={`${isMobile || scrollTop > 50 ? 'block' : 'hidden'} w-full flex justify-end items-center mr-2 relative`}>
       <AnimatePresence initial={false}>
-        {isVisible ? (
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 0.2 }}
-            exit={{ opacity: 0, scale: 0 }}
-            key="box"
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.2 }}
+          exit={{ opacity: 0, scale: 0 }}
+          key="box"
+        >
+          <div ref={refElement} className={`${toggle ? 'translate-y-0 opacity-100' : 'hidden translate-y-[-30px] opacity-0'
+            } h-auto w-56 rounded-lg dark:bg-neutral-800 bg-neutral-200 absolute z-10 top-20 right-5 transition-all duration-300 ease-in-out`}
           >
-            <div ref={refElement} className={`${toggle ? 'translate-y-0 opacity-100' : 'hidden translate-y-[-30px] opacity-0'
-              } h-auto w-56 rounded-lg dark:bg-neutral-800 bg-neutral-200 absolute z-10 top-20 right-5 transition-all duration-300 ease-in-out`}
-            >
-              <Menu direction={true} className='transition-transform delay-200 translate-y-3' setToggleMenu={setToggle} toggle={toggle} />
-            </div>
-          </motion.div>
-        ) : null}
+            <Menu direction={true} className='transition-transform delay-200 translate-y-3' setToggleMenu={handleToggle} />
+          </div>
+        </motion.div>
+
       </AnimatePresence>
       <motion.div
         onClick={() => setIsVisible(!isVisible)}
